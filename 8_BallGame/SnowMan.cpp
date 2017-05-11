ceca160ce01fc2b33bfa845596eed7bae4419ccb
Ana Rita Matos <daxtri@hotmail.com>
2017-05-11 13:40:39 +0100

    beep

--- 8_BallGame/SnowMan.cpp
+++ 8_BallGame/SnowMan.cpp
@@ -3,13 +3,40 @@
 #include "SnowMan.h"
 
 void SnowMan::draw(DrawType drawType) {
-	// Desenha o tronco
-	glColor3f(red, green, blue);
-	glTranslatef(posX, posY + size*0.75f, posZ);
-	if (drawType == DrawType::Solid) glutSolidSphere(size*0.75, 60, 60);
-	else glutWireSphere(size*0.75, 10, 10);
+	// Desenha as bolas
+	//TRIANGULO
+	// --> apenas mudei os valores da cor e translate
+	glColor3f(0.3, 0.5, 1);
+	glTranslatef(posX, posY, posZ + 1.5f);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
 
-	// Desenha a cabeça
+	glColor3f(1, 1, 0.0f);
+	glTranslatef(posX + 0.3f, posY + 0.4f, posZ);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
+
+	glColor3f(1, 0, 1);
+	glTranslatef(posX + 0.2f, posY - 0.4f, posZ);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
+
+	glColor3f(0, 1, 1);
+	glTranslatef(posX + 0.3f, posY + 0.45f, posZ);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
+
+	glColor3f(0, 0, 0);
+	glTranslatef(posX + 0.2f, posY - .45f, posZ);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
+
+	glColor3f(0, 0.2, 0.7);
+	glTranslatef(posX - 0.45f, posY + 0.75f, posZ);
+	if (drawType == DrawType::Solid) glutSolidSphere(0.75, 60, 60);
+	else glutWireSphere(0.25, 10, 10);
+
+	/*// Desenha a cabeça
 	glTranslatef(0.0f, size*1.0f, 0.0f);
 	if (drawType == DrawType::Solid) glutSolidSphere(size*0.25, 30, 30);
 	else glutWireSphere(size*0.25, 10, 10);
@@ -25,7 +52,7 @@ void SnowMan::draw(DrawType drawType) {
 
 	// Desenha o nariz
 	glColor3f(1.0f, 0.5f, 0.5f);
-	glutSolidCone(size*0.08, size*0.5, 10, 2);
+	glutSolidCone(size*0.08, size*0.5, 10, 2);*/
 }
 
 void SnowMan::move(float dx, float dy, float dz) {
